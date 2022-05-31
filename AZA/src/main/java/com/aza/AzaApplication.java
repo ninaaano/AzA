90c537e5df484848778eecf7fd2daf1a77510d03
Heaming <meetoohi@gmail.com>
2022-05-31 20:18:10 +0900

    CREATE : 22/05/31

---
+++ AZA/src/main/java/com/aza/AzaApplication.java
@@ -0,0 +1,13 @@
+package com.aza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzaApplication.class, args);
+	}
+
+}
