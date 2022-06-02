faaeec448833a7b8e5561cf8cb923f7967afa0c7
MY <fourp@naver.com>
2022-06-02 14:21:37 +0900

    MY : 2022.06.02 test commit; characters B/L(테스트 전), exam B/L 작성 중

--- AZA/src/test/java/com/aza/students/test2/StudentsCharacterTest.java
+++ AZA/src/test/java/com/aza/students/test2/StudentsCharacterTest.java
@@ -1,26 +1,18 @@
-package com.aza.students.test;
+package com.aza.students.test2;
 
 import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.transaction.annotation.Transactional;
+
 
 import com.aza.service.domain.Students;
 import com.aza.service.students.StudentsService2;
 
-@WebAppConfiguration
-@RunWith(SpringRunner.class)
-@ContextConfiguration(locations = {"classpath:config/context-aspect.xml",
-									"classpath:config/context-common.xml",
-									"classpath:config/context-mybatis.xml",
-									"classpath:config/context-transaction.xml"})
+
+
 @SpringBootTest
 public class StudentsCharacterTest {
 	
@@ -35,6 +27,8 @@ public class StudentsCharacterTest {
 		
 		students = studentsService.getStudentsCharacter(1002);
 		Assert.assertEquals("student21", students.getStudentId());
+		System.out.println(students.toString());
+
 		
 	}
 }
