764d48f1079d3251008b18a9bfed5459cb2e1881
E-greene <phoex1106@gmail.com>
2022-06-03 10:36:35 +0900

    YS : 2022/06/03 10:36 마스터머지 완료하고 커밋

--- AZA/src/main/java/com/aza/service/lesson/impl/LessonServiceImpl.java
+++ AZA/src/main/java/com/aza/service/lesson/impl/LessonServiceImpl.java
@@ -1,6 +1,6 @@
 package com.aza.service.lesson.impl;
 
-<<<<<<< HEAD
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -129,92 +129,6 @@ public class LessonServiceImpl implements LessonService {
 	public void deleteLessonBook(String isbn) throws Exception {
 		// TODO Auto-generated method stub
 		lessonDao.deleteLessonBook(isbn);
-=======
-import java.util.Map;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Service;
-
-import com.aza.common.Search;
-import com.aza.service.domain.Lesson;
-import com.aza.service.domain.Schedule;
-
-@Service("LessonServiceImpl")
-public class LessonServiceImpl implements LessonService {
-
-	@Autowired
-	@Qualifier("LessonDaoImpl")
-	private LessonDao lessonDao;
-	public void setLessonDao(LessonDao lessonDao) {
-		this.lessonDao = lessonDao;
-	}
-	
-	public LessonServiceImpl() {
-		// TODO Auto-generated constructor stub
-		System.out.println("====lessonServiceImpl=====");
-		System.out.println(this.getClass());
-	}
-
-	@Override
-	public void addLesson(Lesson lesson) throws Exception {
-		// TODO Auto-generated method stub
-		lessonDao.addLesson(lesson);
-	}
-
-	@Override
-	public Lesson getLesson(String lessonCode) throws Exception {
-		// TODO Auto-generated method stub
-		return lessonDao.getLesson(lessonCode);
-	}
-
-	@Override
-	public void updateLesson(Lesson lesson) throws Exception {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public Lesson deleteLesson(String lessonCode) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Map<String, Object> listLesson(Search search, String lessonName) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void addLessonSchedule(Schedule schedule) throws Exception {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public Schedule getLessonSchedule(int scheduleCode) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void updateLessonSchedule(Schedule schedule) throws Exception {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public Schedule deleteLessonSchedule(int scheudleCode) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Map<String, Object> listLessonSchedule(Search search) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
->>>>>>> refs/heads/HM
 	}
 
 }
