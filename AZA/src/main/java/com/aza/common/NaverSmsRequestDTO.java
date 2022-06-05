1d69b4c8cbe788002d8e0ff31c2ca48a47020ffd
MY <fourp@naver.com>
2022-06-05 09:18:37 +0900

    MY : 2022/06/05 주말 commit

--- AZA/src/main/java/com/aza/common/NaverSmsRequestDTO.java
+++ AZA/src/main/java/com/aza/common/NaverSmsRequestDTO.java
@@ -2,43 +2,7 @@ package com.aza.common;
 
 import java.util.List;
 
-<<<<<<< HEAD
-import lombok.Data;
 
-@Data 
-public class NaverSmsRequestDTO {
-	
-	private String reserveTime;
-	private String type; 
-	private String contentType; 
-	private String countryCode; 
-	private String from; 
-	private String content; 
-	private List<NaverSmsMessageRequestDTO> messages;
-	
-	
-	public NaverSmsRequestDTO(String type, String contentType, String countryCode, String from, String content,
-			List<NaverSmsMessageRequestDTO> messages) {
-		this.type = type;
-		this.contentType = contentType;
-		this.countryCode = countryCode;
-		this.from = from;
-		this.content = content;
-		this.messages = messages;
-	}
-	
-	public NaverSmsRequestDTO(String reserveTime, String type, String contentType, String countryCode, String from, String content,
-			List<NaverSmsMessageRequestDTO> messages ) {
-		this.reserveTime = reserveTime;
-		this.type = type;
-		this.contentType = contentType;
-		this.countryCode = countryCode;
-		this.from = from;
-		this.content = content;
-		this.messages = messages;
-		
-	}
-=======
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 
@@ -66,9 +30,7 @@ public class NaverSmsRequestDTO {
 		this.content = content;
 		this.messages = messages;
 	}
-	
 
->>>>>>> refs/heads/MJ
 	
 	
 
--- AZA/src/main/java/com/aza/service/payment/impl/PaymentServiceImpl.java
+++ AZA/src/main/java/com/aza/service/payment/impl/PaymentServiceImpl.java
@@ -1,35 +1,71 @@
 package com.aza.service.payment.impl;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
 
 import com.aza.common.Search;
 import com.aza.service.domain.Payment;
+import com.aza.service.payment.PaymentDao;
 import com.aza.service.payment.PaymentService;
 
+@Service
 public class PaymentServiceImpl implements PaymentService {
 
+	@Autowired
+	@Qualifier("paymentDaoImpl")
+	private PaymentDao paymentDao;
+	
+	public void setPaymentDao(PaymentDao paymentDao) {
+		this.paymentDao = paymentDao;
+	}
+	
+	public PaymentServiceImpl() {
+		System.out.println(this.getClass());
+	}
+	
 	@Override
 	public Payment getPayment(int payCode) throws Exception {
 		// TODO Auto-generated method stub
-		return null;
+		return paymentDao.getPayment(payCode);
 	}
 
 	@Override
 	public void updatePayment(Payment payment) throws Exception {
 		// TODO Auto-generated method stub
-		
+		paymentDao.updatePayment(payment);
 	}
 
 	@Override
 	public void addPayment(Payment payment) throws Exception {
 		// TODO Auto-generated method stub
+		paymentDao.addPayment(payment);
+	}
+	
+	@Override
+	public void deletePayment(int payCode) throws Exception {
+		// TODO Auto-generated method stub
+		paymentDao.deletePayment(payCode);
 		
 	}
-
+	
+	
 	@Override
-	public List<Payment> listPayment(Search search) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+	public Map<String, Object> listPayment(Search search) throws Exception {
+		List<Payment> list = paymentDao.listPayment(search);
+		
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("list", list);
+		
+		return map;
 	}
+
+
+
+
 
 }
