98660cec4c31644983cf4b9eaae532817181c9c7
akh98 <akh1216@gmail.com>
2022-06-21 18:50:10 +0900

    KH 2022/06/21 18:50 혜미가 common 연결함!! 고생했어(미연)

--- AZA/src/main/java/com/aza/web/lesson/LessonController.java
+++ AZA/src/main/java/com/aza/web/lesson/LessonController.java
@@ -28,6 +28,7 @@ import com.aza.service.domain.Lesson;
 import com.aza.service.domain.User;
 import com.aza.service.lesson.LessonService;
 
+
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @Controller
 @RequestMapping("/lesson/*")
